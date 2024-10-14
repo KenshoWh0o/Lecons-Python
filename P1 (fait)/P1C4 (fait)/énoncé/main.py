8baa773b6ac89439233e072bcfc6b99af950b922
Kensho <ari.felix0612@gmail.com>
2024-10-14 19:59:07 +0200

    Update

--- P1 (fait)/P1C4 (fait)/énoncé/main.py
+++ P1 (fait)/P1C4 (fait)/énoncé/main.py
@@ -1,4 +1,16 @@
 # Écrivez votre code ici !
+
+# Création de la variable nom
 nom = "Ari"
-age = 17 + 10
+
+# Création de la variable age
+age = 17 
+
+# Affichage d'une phrase
 print(f"Bonjour ! Je m'appelle {nom} et j'ai {age} ans !")
+
+# Modification de la valeur age
+age = 17 + 10
+
+# Affichage d'une phrase à nouveau
+print(f"Bonjour ! Je m'appelle {nom} et j'ai {age} ans maintenant !")
